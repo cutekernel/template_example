92964673e03a8c0e9dcc44ca01cb9e4be969ccb4
cutekernel <74272629+cutekernel@users.noreply.github.com>
2020-12-01 12:55:49 -0500

    applied dos2unix to *.sh

--- files/start-0.sh
+++ files/start-0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Run this file from inside the ansible server container ./start.sh
+#  Run this file from inside the ansible server container ./start.sh
 ansible nodes -m ping -i fips_testing/nodes_inventory.yml -k -K
